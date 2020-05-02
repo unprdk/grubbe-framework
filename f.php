314c716e4ced22aec969b516673187d0c90acda7
Casper Weiss Bang <master@thecdk.net>
2020-05-02 21:44:23 +0200

    Added docstring

--- f.php
+++ f.php
@@ -1,1 +1,7 @@
+/* Grubbe Framework
+ * A micro PHP framework. It's extensively documented
+ * and follows best practice patterns.
+ * :copyright: 2010 Pallets
+ * :license: BSD-3-Clause
+ */
 <?php
