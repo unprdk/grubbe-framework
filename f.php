243b7a51de571a4470c9f0aff1fca4ffd95a4f6a
Jonas Boserup <jonas@iboserup.dk>
2023-01-29 19:45:59 +0100

    Fix output

--- f.php
+++ f.php
@@ -1,7 +1,7 @@
+<?php
 /* Grubbe Framework
  * A micro PHP framework. It's extensively documented
  * and follows best practice patterns.
  * :copyright: 2020 Unge Programmører
  * :license: MIT License
  */
-<?php
